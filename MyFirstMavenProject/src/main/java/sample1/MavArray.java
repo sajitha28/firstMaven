b208da1e7d6a698bad048a5991579cb5bd050920
sajit <sajit@LAPTOP-L3IGT96C>
2023-07-25 21:26:34 -0400

    first commit

---
+++ MyFirstMavenProject/src/main/java/sample1/MavArray.java
@@ -0,0 +1,28 @@
+package sample1;
+
+public class MavArray {
+
+			// TODO Auto-generated method stub
+		public static void main(String args[])
+		 {
+			 char arr[]={'a','b','c','d'};
+			 System.out.println(arr[2]);
+			 System.out.println(arr[3]);
+			 String arr1[]=new String[5];
+			 arr1[0]="one";
+			 arr1[1]="two";
+			 arr1[2]="three";
+			 arr1[3]="four";
+			 arr1[4]="five";
+			 System.out.println(arr1[3]);
+			 for(int i=0;i<=4;i++)
+			 {
+				System.out.println(arr1[i]);
+			 }
+			 
+			 double arr2[]={5.5,9.2,3.66,5.8,9.63};
+			 System.out.println(arr2.length);
+		 
+	}
+
+}
